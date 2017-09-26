f1032a65cea480d3afe6c02c877de2e03cf90ed1
kreczko <lkreczko@googlemail.com>
2017-09-26 15:25:39 +0100

    added proper MET producer

--- cmsl1t/producers/met.py
+++ cmsl1t/producers/met.py
@@ -1,5 +1,8 @@
 import math
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class MET(object):
@@ -50,3 +53,48 @@ def l1MetNot28HF(caloTowerIphis, caloTowerIetas, caloTowerIets):
         caloTowerIets,
         exclude=lambda towerIeta: abs(towerIeta) == 28
     )
+
+
+class Producer(object):
+
+    METHODS = {
+        'default': recalcMET,
+        'l1Met28Only': l1Met28Only,
+        'l1MetNot28': l1MetNot28,
+        'l1MetNot28HF': l1MetNot28HF,
+    }
+
+    INPUT_ORDER = ['phi', 'eta', 'et']
+
+    def __init__(self, inputs, outputs, params):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.params = params
+
+        self.prefix = self.inputs[0].replace('*', '')
+        if params and 'method' in params:
+            self.method = Producer.METHODS[params['method']]
+        else:
+            msg = 'Could not find specified MET method, using default.'
+            logger.warn(msg)
+            self.method = Producer.METHODS['default']
+        self.outputCollection = self.outputs[0]
+
+        if not self._check_inputs():
+            logger.error('Unexpected input order.')
+            logger.error('Expected order' + ','.join(Producer.INPUT_ORDER))
+            logger.error('Got' + ','.join(self.input))
+
+    def _check_inputs(self):
+        for o, i in zip(self.INPUT_ORDER, self.inputs):
+            if not i.endswith(o):
+                return False
+        return True
+
+    def produce(self, event):
+        if hasattr(event, self.outputCollection):
+            return True
+        variables = [event[i] for i in self.inputs]
+        met = self.method(*variables)
+        setattr(event, self.outputCollection, met)
+        return True
