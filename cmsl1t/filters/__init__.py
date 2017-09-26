740c690ec3eefefd1990b598d7874fd24028a043
kreczko <lkreczko@googlemail.com>
2017-09-26 15:30:04 +0100

    dirty fix to muonfilter

--- cmsl1t/filters/__init__.py
+++ cmsl1t/filters/__init__.py
@@ -1,9 +1,12 @@
 
 
-def muonfilter(muons):
+def muonfilter(event, columns=['Muon_et', 'Muon_iso', 'Muon_isLooseMuon']):
     passes = False
-    for muon in muons:
-        if muon.pt > 20 and muon.iso <= 0.1 and bool(muon.isLooseMuon):
+    mPts = event[columns[0]]
+    mIsos = event[columns[1]]
+    mIDs = event[columns[2]]
+    for (pt, iso, mID) in zip(mPts, mIsos, mIDs):
+        if pt > 20 and iso <= 0.1 and bool(mID):
             passes = True
             break
     return passes
