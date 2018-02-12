6c0551613385a2dfb382d0d5e033268604948e2c
kreczko <lkreczko@googlemail.com>
2018-02-12 16:42:34 +0000

    added draft for batch tests

---
+++ test/batch/test_lsf.py
@@ -0,0 +1,8 @@
+bjobs_test_out = """
+JOBID     USER    STAT  QUEUE      FROM_HOST   EXEC_HOST   JOB_NAME   SUBMIT_TIME
+113     user1   PEND  normal     hostA                   myjob     Jun 17 16:15
+111     user2   RUN   normal     hostA       hostA       myjob     Jun 14 15:13
+110     user1   RUN   normal     hostB       hostA       myjob     Jun 12 05:03
+104     user3   RUN   normal     hostA       hostC       myjob     Jun 11 13:18
+"""
+# alias bjobs='echo "JOBID     USER    STAT  QUEUE      FROM_HOST   EXEC_HOST   JOB_NAME   SUBMIT_TIME";echo "113     user1   PEND  normal     hostA                   myjob     Jun 17 16:15"'
