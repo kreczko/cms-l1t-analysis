2b5e19be157a3734386011cefa1fd018347dab9a
kreczko <lkreczko@googlemail.com>
2018-02-12 16:48:42 +0000

    added common descriptors for batch module

---
+++ cmsl1t/batch/common.py
@@ -0,0 +1,11 @@
+class Status:
+    CREATED = 'CREATED'
+    PENDING = 'PENDING'
+    RUNNING = 'RUNNING'
+    FAILED = 'FAILED'
+    FINISHED = 'FINISHED'
+    UNKNOWN = 'UNKNOWN'
+
+class Batch:
+    lsf = 'LSF'
+    condor = 'HTCondor'
