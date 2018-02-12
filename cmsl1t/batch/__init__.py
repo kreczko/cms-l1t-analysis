84e35ce0bdd0bc9b7e7a39930ff806bc7eca2095
kreczko <lkreczko@googlemail.com>
2018-02-12 16:50:24 +0000

    setting aliases for batch.*

--- cmsl1t/batch/__init__.py
+++ cmsl1t/batch/__init__.py
@@ -1,5 +1,12 @@
 from textwrap import dedent
 
+from .common import Status, Batch
+
+from .condor import submit as condor_submit
+from .condor import get_status as condor_status
+from .lsf import submit as lsf_submit
+from .lsf import get_status as lsf_status
+
 
 def get_run_script(setup_script, shared_fs=True):
     project_root = os.environ["PROJECT_ROOT"]
@@ -20,3 +27,35 @@ def get_run_script(setup_script, shared_fs=True):
         setup_script=setup_script,
     )
     return run_script_contents
+
+
+def prepare_input_file_groups(input_ntuples, files_per_job):
+    file_lists = []
+    current_list = []
+    for infile in input_ntuples:
+        if not infile.startswith("root:"):
+            infile = os.path.realpath(infile)
+        current_list.append(infile)
+
+        # Is the current list full?
+        if len(current_list) >= files_per_job:
+            file_lists.append(current_list)
+            current_list = []
+
+    # Even if the last list had fewer files than needed, make sure to use this
+    # too
+    if current_list:
+        file_lists.append(current_list)
+
+    return file_lists
+
+__all__ = [
+    'Status',
+    'Batch',
+    'condor_submit',
+    'condor_status',
+    'get_run_script',
+    'prepare_input_file_groups',
+    'lsf_submit',
+    'lsf_status',
+]
