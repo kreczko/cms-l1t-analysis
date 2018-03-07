977452bf636b901cdf488a7379e3a4a261982496
kreczko <lkreczko@googlemail.com>
2018-03-07 15:17:32 +0000

    moved most common batch functions to cmsl1t.batch.common

--- cmsl1t/batch/__init__.py
+++ cmsl1t/batch/__init__.py
@@ -1,62 +1,27 @@
 import os
 from textwrap import dedent
 
-from .common import Status, Batch
+from .common import Batch, create_run_script, create_info_file, \
+    get_config_name_template, prepare_jobs, prepare_output_folders, Status
 
 from .condor import submit as condor_submit
 from .condor import get_status as condor_status
+from .condor import resubmit as condor_resubmit
 from .lsf import submit as lsf_submit
 from .lsf import get_status as lsf_status
 
 
-def get_run_script(setup_script, project_root, shared_fs=True):
-    run_script_contents = [
-        '#!/usr/bin/env bash',
-        'pushd {project_root}',
-        'source {setup_script}',
-        'popd',
-        'cmsl1t -c "$1"',
-        '',
-    ]
-    if not shared_fs:
-        # infer project root
-        pass
-    run_script_contents = '\n'.join(run_script_contents)
-    run_script_contents = run_script_contents.format(
-        project_root=project_root,
-        setup_script=setup_script,
-    )
-    return run_script_contents
-
-
-def prepare_input_file_groups(input_ntuples, files_per_job):
-    file_lists = []
-    current_list = []
-    for infile in input_ntuples:
-        if not infile.startswith("root:"):
-            infile = os.path.realpath(infile)
-        current_list.append(infile)
-
-        # Is the current list full?
-        if len(current_list) >= files_per_job:
-            file_lists.append(current_list)
-            current_list = []
-
-    # Even if the last list had fewer files than needed, make sure to use this
-    # too
-    if current_list:
-        file_lists.append(current_list)
-
-    return file_lists
-
-
 __all__ = [
-    'Status',
     'Batch',
-    'condor_submit',
+    'condor_resubmit',
     'condor_status',
-    'get_run_script',
-    'prepare_input_file_groups',
-    'lsf_submit',
+    'condor_submit',
+    'create_info_file',
+    'create_run_script',
+    'get_config_name_template',
+    'prepare_jobs',
+    'prepare_output_folders',
+    'Status',
     'lsf_status',
+    'lsf_submit',
 ]
