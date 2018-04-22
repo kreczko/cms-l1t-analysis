c02fb43a5f76c2f546f2f7be067e0adf332cd072
Aaron <aaron.bundock@cern.ch>
2018-04-22 16:28:22 +0200

    Remove unnecessary imports

--- cmsl1t/plotting/rate_vs_pileup.py
+++ cmsl1t/plotting/rate_vs_pileup.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
-import numpy as np
 from cmsl1t.plotting.base import BasePlotter
 from cmsl1t.hist.hist_collection import HistogramCollection
-from cmsl1t.hist.factory import HistFactory
 import cmsl1t.hist.binning as bn
 from cmsl1t.utils.draw import draw, label_canvas
 from cmsl1t.recalc.resolution import get_resolution_function
