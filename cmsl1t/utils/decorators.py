c68e65455b68b8328f90de6ce1d655de4e235669
kreczko <lkreczko@googlemail.com>
2018-04-30 16:06:52 +0100

    added deprecation decorator

---
+++ cmsl1t/utils/decorators.py
@@ -0,0 +1,23 @@
+'''
+From http://code.activestate.com/recipes/577819-deprecated-decorator/
+'''
+
+import warnings
+import functools
+
+
+def deprecated(replacement=None):
+    def outer(fun):
+        msg = "{0} is deprecated".format(fun.__name__)
+        if replacement is not None:
+            msg += "; use {0} instead".format(replacement)
+        if fun.__doc__ is None:
+            fun.__doc__ = msg
+
+        @functools.wraps(fun)
+        def inner(*args, **kwargs):
+            warnings.warn(msg, category=DeprecationWarning, stacklevel=2)
+            return fun(*args, **kwargs)
+
+        return inner
+    return outer
