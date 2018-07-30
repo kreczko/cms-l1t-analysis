d64a488b4648d3f730389a3b75cac3a084f50fdf
kreczko <lkreczko@googlemail.com>
2018-07-30 17:04:48 +0100

    added integration tests to travis

--- ci/integration-test.sh
+++ ci/integration-test.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# configs='all2017 demo efficiencies gen offline_met_studies rates rateVsPU study_tower28_met weekly_checks'
-configs='all2017 demo efficiencies offline_met_studies rates rateVsPU study_tower28_met weekly_checks'
-configs='rates rateVsPU study_tower28_met weekly_checks'
-
-for c in $configs
-do
-  set -x
-  cmsl1t -c config/$c.yaml
-  set +x
-done
+set -x
+cmsl1t -c config/${CI_RUN_CONFIG}.yaml
+set +x
--- ci/test.sh
+++ ci/test.sh
@@ -5,6 +5,13 @@ cd ${CODE_PATH}
 source bin/env.sh
 make setup
 ls -l build
-make test
 
-./ci/integration-test.sh
+if [ "x${CI_RUN_TEST}"== "xUNIT" ]
+then
+  make test
+fi
+
+if [ "x${CI_RUN_TEST}"== "xINTEGRATION" ]
+then
+  ./ci/integration-test.sh
+fi
