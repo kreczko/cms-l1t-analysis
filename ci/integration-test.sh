3eed49df46bdefa17ff97cc7a4600e0860e2034f
kreczko <lkreczko@googlemail.com>
2018-07-30 15:54:49 +0100

    updated muon filter to new eventreader

--- ci/integration-test.sh
+++ ci/integration-test.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-configs='all2017 demo efficiencies gen offline_met_studies rates rateVsPU study_tower298_met weekly_checks'
+# configs='all2017 demo efficiencies gen offline_met_studies rates rateVsPU study_tower28_met weekly_checks'
+configs='all2017 demo efficiencies offline_met_studies rates rateVsPU study_tower28_met weekly_checks'
+configs='rates rateVsPU study_tower28_met weekly_checks'
 
 for c in $configs
 do
+  set -x
   cmsl1t -c config/$c.yaml
+  set +x
 done
