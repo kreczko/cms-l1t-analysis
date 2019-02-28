75018918dcd7caa14c4883053ae14623fc98041f
Lukasz Kreczko <lkreczko@googlemail.com>
2019-02-28 10:25:14 +0100

    implemented bulk CLI submission for HTCondor

--- cmsl1t/batch/condor.py
+++ cmsl1t/batch/condor.py
@@ -1,4 +1,3 @@
-import htcondor
 import logging
 import os
 import socket
@@ -19,62 +18,85 @@ CONDOR_STATUS = [
 
 
 def submit(config_files, batch_directory, batch_log_dir, run_script):
+    import htcondor
     logger.info("Will submit {0} jobs".format(len(config_files)))
     schedd = htcondor.Schedd()
     results = []
-    with schedd.transaction() as txn:
-        for i, cfg in enumerate(config_files):
-            result = __submit_one(
-                txn, i, cfg, batch_directory, batch_log_dir, run_script
-            )
-            results.append(result)
+
+    job_cfgs = []
+    for i, cfg in enumerate(config_files):
+        job_cfgs.append(__create_job_cfg(i, cfg, batch_directory, batch_log_dir, run_script))
+
+    if 'cern.ch' in socket.gethostname():
+        results = list(_submit_via_command_line(job_cfgs, config_files, batch_directory))
+    else:
+        with schedd.transaction() as txn:
+            for job_cfg, cfg in zip(job_cfgs, config_files):
+                result = __submit_one(txn, job_cfgs, cfg)
+                results.append(result)
 
     return results
 
 
-def __submit_one(txn, index, config_file, batch_directory, batch_log_dir, run_script):
+def __create_job_cfg(index, config_file, batch_directory, batch_log_dir, run_script):
     cfg = os.path.realpath(config_file)
     stderr_log = os.path.join(batch_log_dir, 'job_{0}.err'.format(index))
     stdout_log = os.path.join(batch_log_dir, 'job_{0}.out'.format(index))
     job_log = os.path.join(batch_log_dir, 'job_{0}.log'.format(index))
-    job_cfg = dict(
+    return dict(
         executable=run_script,
         arguments="-c {}".format(cfg),
         output=stdout_log,
         error=stderr_log,
         log=job_log,
     )
-    out = None
-    if 'cern.ch' in socket.gethostname():
-        out = _submit_one_console(job_cfg, batch_log_dir)
-    else:
-        sub = htcondor.Submit(job_cfg)
-        out = sub.queue(txn)
+
+
+def __submit_one(txn, job_cfg, cfg):
+    import htcondor
+    sub = htcondor.Submit(job_cfg)
+    out = sub.queue(txn)
     return dict(
         batch_id=int(out),
         batch=Batch.condor,
         config_file=cfg,
-        stderr_log=stderr_log,
-        stdout_log=stdout_log,
-        job_log=job_log,
+        stderr_log=job_cfg['error'],
+        stdout_log=job_cfg['output'],
+        job_log=job_cfg['log'],
         status=Status.CREATED,
     )
 
 
-def _submit_one_console(job_cfg, batch_log_dir):
-    condor_submit_file = os.path.join(batch_log_dir, 'job_{0}.submit'.format(index))
-    with open(condor_submit_file, 'w') as f:
-        content = '\n'.join([k + ' = ' + v for k, v in job_cfg.items()])
-        content += 'queue' + '\n'
+def _submit_via_command_line(job_cfgs, config_files, batch_directory):
+    condor_submit_file = os.path.join(batch_directory, 'job.submit')
+    with open(condor_submit_file, 'w+') as f:
+        content = '\n'
+        for job_cfg in job_cfgs:
+            content += '\n'.join([k + ' = ' + v for k, v in job_cfg.items()])
+            content += '\n' + 'queue' + '\n'
         f.write(content)
-    condor_submit = local('condor_submit')
-    out = condor_submit[condor_submit_file]
-    return __parse_condor_submit_output(out)
+    condor_submit = local['condor_submit']
+    out = condor_submit(condor_submit_file)
+    batch_ids = list(_parse_condor_submit_output(out))
+    for i, job_cfg in enumerate(job_cfgs):
+        yield dict(
+            # batch_id=int(out),
+            batch_id=batch_ids[i],
+            batch=Batch.condor,
+            config_file=config_files[i],
+            stderr_log=job_cfg['error'],
+            stdout_log=job_cfg['output'],
+            job_log=job_cfg['log'],
+            status=Status.CREATED,
+        )
 
 
-def __parse_condor_submit_output(out):
-    m = re.search(r"(submitted to cluster) (\d+)\.", out)
-    return int(m.group(2))
+def _parse_condor_submit_output(out):
+    last_line = out.split('\n')[-1]
+    m = re.search(r"(\d+)( .* )(\d+)\.", last_line)
+    n_jobs, _, condor_id = m.groups()
+    for i in range(int(n_jobs)):
+        yield condor_id + '.' + str(i)
 
 
 def get_status(batch_id):
