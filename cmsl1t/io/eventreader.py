3de146cd0cea96bccefc3f48ac81247d80d5f5fa
kreczko <lkreczko@googlemail.com>
2019-05-14 15:32:16 +0100

    added vectorized event loop

--- cmsl1t/io/eventreader.py
+++ cmsl1t/io/eventreader.py
@@ -1,8 +1,12 @@
+import sys
+
 import logging
 import six
+import uproot
 
 import ROOT
 from rootpy.tree import TreeChain
+
 
 from cmsl1t.utils.root_glob import glob
 from cmsl1t.utils.module import load_L1TNTupleLibrary
@@ -50,7 +54,7 @@ def _create_alias_map(ntuple_map):
 
 class EventReader(object):
 
-    def __init__(self, input_files, ntuple_map, nevents=-1):
+    def __init__(self, input_files, ntuple_map, nevents=-1, vectorized=False):
         '''
             Reads ntuple_info as defined by bin/create-map-file
         '''
@@ -60,7 +64,15 @@ class EventReader(object):
         self.nevents = nevents
         self._trees = {}
 
-        self._load_trees()
+        self._used_arrays = False
+        self._used_trees = False
+        self._passed_events = 0
+        self._batch_size = 1
+
+        if vectorized:
+            self._load_arrays()
+        else:
+            self._load_trees()
 
     def _load_trees(self):
         for treeName in self._treeNames:
@@ -71,18 +83,95 @@ class EventReader(object):
                     cache=True,
                     events=self.nevents,
                 )
-            except RuntimeError:
-                logger.warn(
+                logger.debug("Successfully loaded {0}".format(treeName))
+            except RuntimeError as e:
+                logger.warning(
                     "Cannot find tree: {0} in input file".format(treeName))
+                logger.error(e)
+                if treeName in self._trees:
+                    logger.warning('DELETING TREE')
+                    del self._trees[treeName]
                 continue
+        self._used_trees = True
+
+    def _load_arrays(self):
+        for treeName in self._treeNames:
+            try:
+                self._trees[treeName] = uproot.iterate(
+                    self.input_files,
+                    treeName,
+                    entrysteps=self._batch_size,
+                )
+                logger.debug("Successfully loaded {0}".format(treeName))
+            except RuntimeError as e:
+                logger.warning(
+                    "Cannot find tree: {0} in input file".format(treeName))
+                logger.error(e)
+                if treeName in self._trees:
+                    logger.warning('DELETING TREE')
+                    del self._trees[treeName]
+                continue
+        self._used_arrays = True
 
     def __contains__(self, name):
         return name in self._aliasMap.keys()
 
     def __iter__(self):
         # event loop
-        for trees in six.moves.zip(*six.itervalues(self._trees)):
-            yield Event(self._trees, self._aliasMap)
+        try:
+            if self._used_trees:
+                for trees in six.moves.zip(*six.itervalues(self._trees)):
+                    yield Event(self._trees, self._aliasMap)
+            if self._used_arrays:
+                for treeGen in six.moves.zip(*six.itervalues(self._trees)):
+                    data = dict(six.moves.zip(self._trees, treeGen))
+                    yield UprootEvent(data, self._aliasMap, batch_size=self._batch_size)
+                    self._passed_events += 1
+                    if self._passed_events >= self.nevents:
+                        break
+        except Exception as e:
+            logger.critical("Error when reading data from ROOT file: {}".format(e))
+            sys.exit(-1)
+
+
+class UprootEvent(object):
+
+    def __init__(self, data, mapping, batch_size=1):
+        self._data = data
+        self._map = mapping
+        self._cache = {}
+        self._isPy3 = sys.version_info[0] == 3
+        self._batch_size = batch_size
+
+    def _contruct_cache(self):
+        pass
+
+    def __getattr__(self, name):
+        if name in object.__getattribute__(self, '_cache'):
+            return object.__getattribute__(self, '_cache')[name]
+
+        if name not in object.__getattribute__(self, '_map'):
+            return object.__getattribute__(self, name)
+
+        treeName, treeAttr = object.__getattribute__(self, '_map')[name]
+        if treeName not in self._data:
+            raise RuntimeError("Cannot find tree {0}".format(treeName))
+
+        if treeAttr not in self._data[treeName]:
+            treeAttr = treeAttr.split('.')[-1]
+            if self._isPy3:
+                treeAttr = bytes(treeAttr, 'utf-8')
+            if treeAttr not in self._data[treeName]:
+                raise RuntimeError("Cannot find branch {0} in tree {1}".format(treeAttr, treeName))
+
+        data = self._data[treeName][treeAttr]
+        if self._batch_size == 1:
+            data = data[0]
+        object.__getattribute__(self, '_cache')[name] = data
+        return data
+
+    def __getitem__(self, name):
+        return object.__getattribute__(self, '__getattr__')(name)
 
 
 class Event(object):
