430154fe660779b8284dab65e96cabbc9acc5fe5
kreczko <lkreczko@googlemail.com>
2019-05-14 15:31:48 +0100

    added tests for eventreader event loop

--- test/io/test_eventreader.py
+++ test/io/test_eventreader.py
@@ -1,6 +1,10 @@
-import pytest
-from cmsl1t.io.eventreader import Event
+import os
+
 from collections import namedtuple
+import pytest
+
+import cmsl1t
+from cmsl1t.io.eventreader import Event, EventReader
 
 
 @pytest.fixture
@@ -32,6 +36,20 @@ def mapping():
     }
 
 
+@pytest.fixture
+def test_data():
+    path = os.path.join(cmsl1t.PROJECT_ROOT, 'test/data/2kmu.root')
+    return [path]
+
+
+@pytest.fixture
+def ntuple_map():
+    import yaml
+    path = os.path.join(cmsl1t.PROJECT_ROOT, 'config/ntuple_content_2kmu.yaml')
+    with open(path) as f:
+        return yaml.load(f)
+
+
 def test_mapping(caloTree, mapping):
     trees = {
         'l1CaloTowerEmuTree/L1CaloTowerTree': caloTree,
@@ -44,3 +62,18 @@ def test_mapping(caloTree, mapping):
     assert observed == expected
     observed = event.emu_CaloTP_ecalTPCaliphi
     assert observed == expected
+
+
+def test_event_loop(ntuple_map, test_data):
+    nevents = 10
+    reader = EventReader(input_files=test_data, ntuple_map=ntuple_map, nevents=nevents, vectorized=True)
+    events = list(reader)
+    assert len(events) == nevents
+    for event in events:
+        assert event['CaloTP_ecalTPCaliphi'] is not None
+        if reader._batch_size > 1:
+            # we are dealing with arrays of arrays
+            assert len(event['CaloTP_ecalTPCaliphi']) == reader._batch_size
+        else:
+            # like a simple for-loop
+            assert len(event['CaloTP_ecalTPCaliphi']) > 1
