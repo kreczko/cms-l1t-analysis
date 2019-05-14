ef2026f50629e00013a14ce3b1aadb7f53c0ed1c
kreczko <lkreczko@googlemail.com>
2019-05-14 17:01:50 +0100

    CI: updated miniconda version to pick up new ROOT from conda-forge

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -6,7 +6,7 @@ else
   rm -rf ${CMSL1T_CONDA_PATH}
   mkdir -p $HOME/download
   if [[ -d $HOME/download/miniconda.sh ]] ; then rm -rf $HOME/download/miniconda.sh ; fi
-  wget -c https://repo.continuum.io/miniconda/Miniconda2-4.1.11-Linux-x86_64.sh -O $HOME/download/miniconda.sh
+  wget -c https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh -O $HOME/download/miniconda.sh
   chmod +x $HOME/download/miniconda.sh
   $HOME/download/miniconda.sh -b -p ${CMSL1T_CONDA_PATH}
 fi
