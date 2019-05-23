9674f1deff38ca7dd8a1d1109655a754ffd1fb29
kreczko <lkreczko@googlemail.com>
2019-05-23 15:41:58 +0100

    added vectorized filters

--- cmsl1t/filters/__init__.py
+++ cmsl1t/filters/__init__.py
@@ -1,8 +1,52 @@
+import numpy as np
+
 from .muonfilter import muonfilter
 from .luminosity import LuminosityFilter
 from .pfMetFilter import pfMetFilter
 
+
+def __combine_with_AND_vectorized(filters, size):
+    # for vectorized filters, this dimension are events
+    for f in filters:
+        yield np.all(f)
+
+
+def __combine_with_OR_vectorized(filters, size):
+    # for vectorized filters, this dimension are events
+    for f in filters:
+        yield np.any(f)
+
+
+def combine_with_AND(filters):
+    size = np.size(filters[0])
+    if size > 1:
+        return list(__combine_with_AND_vectorized(filters, size))
+    result = True
+    for f in filters:
+        result = result & f
+    return result
+
+
+def combine_with_OR(filters):
+    size = np.size(filters[0])
+    if size > 1:
+        return list(__combine_with_OR_vectorized(filters, size))
+    result = False
+    for f in filters:
+        result = result | f
+    return result
+
+
+def create_event_mask(filters, method=combine_with_AND):
+    if not filters:
+        return []
+    return method(filters)
+
+
 __all__ = [
+    'combine_with_AND',
+    'combine_with_OR',
+    'create_event_mask',
     'muonfilter',
     'LuminosityFilter',
     'pfMetFilter',
---
+++ test/filters/test_masks.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pytest
+
+from cmsl1t.filters import create_event_mask, combine_with_AND, combine_with_OR
+
+
+scalar_filters_all_passing = [True, True, True]
+scalar_filters_all_failing = [False, False, False]
+scalar_filters_all_but_one_failing = [False, True, False]
+vector_filters_all_passing = [
+    np.array([True, True, True]),
+    np.array([True, True, True]),
+    np.array([True, True, True]),
+    np.array([True, True, True]),
+]
+vector_filters_all_failing = [
+    np.array([False, False, False]),
+    np.array([False, False, False]),
+    np.array([False, False, False]),
+    np.array([False, False, False]),
+]
+
+vector_filters_all_but_one_failing = [
+    np.array([False, False, False]),
+    np.array([False, True, False]),
+    np.array([False, False, False]),
+    np.array([False, False, False]),
+]
+
+
+@pytest.mark.parametrize('filters, expected', [
+    (scalar_filters_all_passing, True),
+    (scalar_filters_all_failing, False),
+    (scalar_filters_all_but_one_failing, False),
+    (vector_filters_all_passing, [True, True, True, True]),
+    (vector_filters_all_failing, [False, False, False, False]),
+    (vector_filters_all_but_one_failing, [False, False, False, False]),
+]
+)
+def test_combine_with_AND(filters, expected):
+    result = combine_with_AND(filters)
+    assert np.size(result) == np.size(expected)
+    assert result == expected
+
+
+@pytest.mark.parametrize('filters, expected', [
+    (scalar_filters_all_passing, True),
+    (scalar_filters_all_failing, False),
+    (scalar_filters_all_but_one_failing, True),
+    (vector_filters_all_passing, [True, True, True, True]),
+    (vector_filters_all_failing, [False, False, False, False]),
+    (vector_filters_all_but_one_failing, [False, True, False, False]),
+]
+)
+def test_combine_with_OR(filters, expected):
+    result = combine_with_OR(filters)
+    assert np.size(result) == np.size(expected)
+    assert result == expected
