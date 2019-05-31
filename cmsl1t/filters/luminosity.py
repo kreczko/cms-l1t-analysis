f22fae40c332dd9a6f345442eed6f871fef760cf
kreczko <lkreczko@googlemail.com>
2019-05-31 13:10:00 +0100

    added global filters to cmsl1t

--- cmsl1t/filters/luminosity.py
+++ cmsl1t/filters/luminosity.py
@@ -1,6 +1,7 @@
 import json
 import six
 import six.moves.urllib as urllib
+import numba
 import numpy as np
 
 
@@ -40,7 +41,29 @@ class LuminosityFilter(object):
             lumis = _expand_lumi_ranges(lumi_ranges)
             tuples = map(lambda x: (int(run), x), lumis)
             self.valid_lumi_sections.extend(tuples)
-        self.valid_lumi_sections = set(self.valid_lumi_sections)
+        self.valid_lumi_sections = np.array(self.valid_lumi_sections)
 
-    def __call__(self, run, lumi):
-        return (run, lumi) in self.valid_lumi_sections
+    def __call__(self, event):
+        # TODO: wrong way around!
+        # need to create a new array from the event.run and event.lumi
+        # https://stackoverflow.com/questions/35091879/merge-2-arrays-vertical-to-tuple-numpy
+        # https://docs.scipy.org/doc/numpy/reference/generated/numpy.isin.html
+        to_test = np.array(list(zip(event.run, event.lumi)))
+        # print(np.size(to_test), np.size(self.valid_lumi_sections))
+        # print(self.valid_lumi_sections)
+        # mask = np.isin(self.valid_lumi_sections, to_test, assume_unique=True)
+        mask = filter_lumis(to_test, self.valid_lumi_sections)
+        print('Lumi filter', to_test, mask, '({})'.format(np.size(mask)))
+        return mask
+
+
+@numba.jit(nopython=True, parallel=True)
+def filter_lumis(ranges_to_test, valid_lumi_sections):
+    result = np.zeros(ranges_to_test.shape[0], dtype=np.uint8)
+    for i in range(valid_lumi_sections.shape[0]):
+        valid_section = valid_lumi_sections[i, :]
+        for j in range(ranges_to_test.shape[0]):
+            to_test = ranges_to_test[j, :]
+            if np.equal(to_test, valid_section).all():
+                result[j] = 1
+    return result
