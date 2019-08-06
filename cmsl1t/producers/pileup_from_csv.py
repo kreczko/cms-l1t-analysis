20f70f5c5a054346ae4145f28c9ee059c9ae340b
kreczko <lkreczko@googlemail.com>
2019-08-06 09:30:46 +0100

    cmsl1t.producers.pileup_from_csv: added tests for (run,lumi) completely not in run_lumi.csv

--- cmsl1t/producers/pileup_from_csv.py
+++ cmsl1t/producers/pileup_from_csv.py
@@ -2,12 +2,11 @@ import numpy as np
 import pandas as pd
 
 from .base import BaseProducer
+from .. import logger
 
 
 def _load_csv(csv_file):
-    lumiMuDict = dict()
-    df = pd.read_csv(csv_file, names=['entry', 'run', 'lumi', 'pileup'], index_col=['run', 'lumi'])
-    return df
+    return pd.read_csv(csv_file, names=['entry', 'run', 'lumi', 'pileup'], index_col=['run', 'lumi'])
 
 
 class Producer(BaseProducer):
@@ -21,5 +20,23 @@ class Producer(BaseProducer):
 
     def produce(self, event):
         variables = [event[i] for i in self._inputs]
+        pileup = np.zeros(np.size(variables[0]))
+        # TODO:
+        """
+          /software/miniconda/envs/hep_py3/lib/python3.6/site-packages/pandas/core/indexing.py:969: FutureWarning:
+          Passing list-likes to .loc or [] with any missing label will raise
+          KeyError in the future, you can use .reindex() as an alternative.
 
-        setattr(event, self._outputs[0], self._data.loc[list(zip(*variables)), 'pileup'].fillna(0).to_numpy())
+          See the documentation here:
+          https://pandas.pydata.org/pandas-docs/stable/indexing.html#deprecate-loc-reindex-listlike
+          return self._getitem_nested_tuple(tup)
+
+          -- Docs: https://docs.pytest.org/en/latest/warnings.html
+        """
+        try:
+            pileup = self._data.loc[list(zip(*variables)), 'pileup'].fillna(0).to_numpy()
+        except KeyError:
+            logger.warning('Could not find any (run, lum) combination in {}'.format(self._run_lumi_file))
+        setattr(event, self._outputs[0], pileup)
+
+        return True
--- test/producers/test_pileup_from_csv.py
+++ test/producers/test_pileup_from_csv.py
@@ -1,21 +1,30 @@
+import numpy as np
 import os
 import pytest
+
 
 import cmsl1t
 from cmsl1t.producers.pileup_from_csv import Producer
 
 from .. import DummyEvent
 
-@pytest.fixture
-def events():
-    return DummyEvent(
-        run=[324980, 324980, 324980, 324980, 324878, 324878, 324878, 1],
-        lumi=[40, 41, 42, 43, 620, 621, 622, 42],
-    )
+
+@pytest.fixture(params=['events', 'events_not_in_csv'])
+def data(request):
+    if request.param == 'events':
+        return DummyEvent(
+            run=[324980, 324980, 324980, 324980, 324878, 324878, 324878, 1],
+            lumi=[40, 41, 42, 43, 620, 621, 622, 42],
+        ), [6.4, 15.6, 36.1, 51.7, 33.4, 33.4, 33.4, 0]
+    if request.param == 'events_not_in_csv':
+        return DummyEvent(
+            run=np.zeros(10),
+            lumi=np.zeros(10),
+        ), [0] * 10
 
 
-def test_pileup_from_csv(events):
-    expected = [6.4, 15.6, 36.1, 51.7, 33.4, 33.4, 33.4, 0]
+def test_pileup_from_csv(data):
+    events, expected = data
     run_lumi_csv = os.path.join(cmsl1t.PROJECT_ROOT, 'run_lumi.csv')
     p = Producer(inputs=['run', 'lumi'], outputs=['pileup_from_csv'], csv_file=run_lumi_csv)
     p.produce(events)
